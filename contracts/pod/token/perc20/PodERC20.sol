dce8a40e4dd28edf5a50c0182510934e670f7524
Naiem <naiem@coti.io>
2026-06-28 16:31:28 +0300

    Move PoD to the contracts folder

--- contracts/pod/token/perc20/PodERC20.sol
+++ contracts/pod/token/perc20/PodERC20.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "../../../utils/mpc/MpcCore.sol";
 import "../../InboxUser.sol";
 import "../../fee/IInboxFeeManager.sol";
 import "../../mpccodec/MpcAbiCodec.sol";
